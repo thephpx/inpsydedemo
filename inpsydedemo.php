b48b67732dc51233925e3b8d73de500b9437d1bf
Faisal Ahmed <thephpx@gmail.com>
2018-02-20 00:58:48 +0600

    composer installed and base class updated

--- inpsydedemo.php
+++ inpsydedemo.php
@@ -7,9 +7,17 @@ Description: This plugin demo wp_nounce
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
+global $wpdb;
+
+use App\Utility;
+use App\Validation;
+use App\Form;
+
 class Inpsydedemo{
 
 	private static $instance;
+
+	private $db;
 
 	private function __construct()
 	{
@@ -38,11 +46,37 @@ class Inpsydedemo{
 
 	}
 
+	public function setDependency($type,&$bject){
+		$this->{$type} = $object;
+	}
+
+	public function initialize(){
+		register_activation_hook( __FILE__, array( 'Inpsydedemo', 'activate' ) );
+		register_deactivation_hook( __FILE__, array( 'Inpsydedemo', 'deactivate' ) );
+	}
+
+	public function activate(){
+		$args = array(
+	      'public' => true,
+	      'label'  => 'inpsyde'
+	    );
+		register_post_type( 'inpsyde', $args );
+	}
+
+	public function deactivate(){
+		
+	}
+
 }
 
 
 if(class_exists('Inpsydedemo')){
 
+	
+
+
 	$inpsydedemo = Inpsydedemo::getInstance();
+	$inpsydedemo->setDependency('db', $wpdb);
+	$inpsydedemo->initialize();
 
 }
