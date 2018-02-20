5163ea23f9920e608f1080905d4e15575a192a00
Faisal Ahmed <thephpx@gmail.com>
2018-02-20 18:53:37 +0600

    nonce form check

--- tests/test-inpsyde.php
+++ tests/test-inpsyde.php
@@ -29,6 +29,6 @@ class InpsydeTest extends WP_UnitTestCase {
 		$form = $inpsydedemo->inpsyde_form();
 		$this->assertNotContains('<input type="hidden" name="inpsyde_form_nonce" value=""/>',$form);
 
-		$this->assertRegExp('input\stype\=\"hidden\"\sname\=\"inpsyde_form_nonce\"\svalue\=\"([a-zA-Z]{5,})\"\/',$form);
+		$this->assertRegExp('/input\stype\=\"hidden\"\sname\=\"inpsyde_form_nonce\"\svalue\=\"([a-zA-Z]{5,})\"\//',$form);
 	}
 }
